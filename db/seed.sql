6ee9ae797cfdb0152b65d058098412c281bc1dd5
jon-shogren <jon.shogren@mailchimp.com>
2022-09-21 07:40:55 -0400

    initial files and directories, database structure

---
+++ db/seed.sql
@@ -0,0 +1,24 @@
+USE employeeDB;
+
+INSERT INTO department (name)
+VALUES ("Engineering");
+INSERT INTO department (name)
+VALUES ("Finance");
+INSERT INTO department (name)
+VALUES ("Sales");
+INSERT INTO department (name)
+VALUES ("Legal");
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Software Engineer", 120000, 1);
+INSERT INTO role (title, salary, department_id)
+VALUES ("Sales Lead", 100000, 3);
+INSERT INTO role (title, salary, department_id)
+VALUES ("Lead Engineer", 150000, 1);
+INSERT INTO role (title, salary, department_id)
+VALUES ("Accountant", 125000, 2);
+INSERT INTO role (title, salary, department_id)
+VALUES ("Legal Team Lead", 250000, 4);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Jon", "Shogren", 1, 3);
